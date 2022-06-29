46ac8fbc8f668a79d34a5de1e13c9447e5180855
AlexandreCR <devalexandrecr@gmail.com>
2022-06-29 16:15:48 -0500

    refactor: initialice viewModel home

--- app/src/main/java/gorda/driver/ui/home/HomeFragment.kt
+++ app/src/main/java/gorda/driver/ui/home/HomeFragment.kt
@@ -7,6 +7,7 @@ import android.view.ViewGroup
 import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.activityViewModels
+import androidx.fragment.app.viewModels
 import androidx.lifecycle.ViewModelProvider
 import androidx.navigation.fragment.findNavController
 import androidx.recyclerview.widget.RecyclerView
@@ -30,8 +31,7 @@ class HomeFragment : Fragment() {
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-        val homeViewModel =
-            ViewModelProvider(this).get(HomeViewModel::class.java)
+        val homeViewModel: HomeViewModel by viewModels()
 
         _binding = FragmentHomeBinding.inflate(inflater, container, false)
         val root: View = binding.root
