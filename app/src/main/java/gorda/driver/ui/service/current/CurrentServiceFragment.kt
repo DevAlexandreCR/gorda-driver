fe2127911f9772ad79658cbddcc7e62c6fcb9d61
AlexandreCR <devalexandrecr@gmail.com>
2022-06-28 19:36:53 -0500

    fix: add onAttach

--- app/src/main/java/gorda/driver/ui/service/current/CurrentServiceFragment.kt
+++ app/src/main/java/gorda/driver/ui/service/current/CurrentServiceFragment.kt
@@ -1,5 +1,6 @@
 package gorda.driver.ui.service.current
 
+import android.content.Context
 import android.os.Bundle
 import android.util.Log
 import android.view.LayoutInflater
@@ -29,25 +30,29 @@ class CurrentServiceFragment : Fragment() {
     private var _binding: FragmentCurrentServiceBinding? = null
     private val currentServiceViewModel: CurrentServiceViewModel by viewModels()
     private val mainViewModel: MainViewModel by activityViewModels()
-    private val btnStatus: Button = binding.btnServiceStatus
-    private val haveArrived = requireActivity().resources.getString(R.string.service_have_arrived)
-    private val startTrip = requireActivity().resources.getString(R.string.service_start_trip)
-    private val endTrip = requireActivity().resources.getString(R.string.service_end_trip)
-
-    private val binding get() = _binding!!
+    private lateinit var btnStatus: Button
+    private lateinit var textName: TextView
+    private lateinit var textPhone: TextView
+    private lateinit var textAddress: TextView
+    private lateinit var textComment: TextView
+    private lateinit var haveArrived: String
+    private lateinit var startTrip: String
+    private lateinit var endTrip: String
+    private lateinit var binding: FragmentCurrentServiceBinding
 
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-        _binding = FragmentCurrentServiceBinding.inflate(inflater, container, false)
+        binding = FragmentCurrentServiceBinding.inflate(inflater, container, false)
         val root: View = binding.root
 
-        val textName: TextView = binding.currentServiceName
-        val textPhone: TextView = binding.currentPhone
-        val textAddress: TextView = binding.currentAddress
-        val textComment: TextView = binding.serviceComment
+        textName = binding.currentServiceName
+        textPhone = binding.currentPhone
+        textAddress = binding.currentAddress
+        textComment = binding.serviceComment
+        btnStatus = binding.btnServiceStatus
         mainViewModel.currentService.observe(viewLifecycleOwner) { service ->
             if (service != null) {
                 setOnClickListener(service)
@@ -67,6 +72,13 @@ class CurrentServiceFragment : Fragment() {
             }
         }
         return root
+    }
+
+    override fun onAttach(context: Context) {
+        super.onAttach(context)
+        haveArrived = context.resources.getString(R.string.service_have_arrived)
+        startTrip = context.resources.getString(R.string.service_start_trip)
+        endTrip = context.resources.getString(R.string.service_end_trip)
     }
 
     private fun setOnClickListener(service: Service) {
