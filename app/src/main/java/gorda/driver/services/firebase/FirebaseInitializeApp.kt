e4020171d14b6748051bf13246dce9003030c373
DevAlexandreCR <devalexandrecr@gmail.com>
2022-08-23 20:45:23 -0500

    config: add env emulators

--- app/src/main/java/gorda/driver/services/firebase/FirebaseInitializeApp.kt
+++ app/src/main/java/gorda/driver/services/firebase/FirebaseInitializeApp.kt
@@ -7,19 +7,19 @@ import gorda.driver.BuildConfig
 
 object FirebaseInitializeApp {
     val auth: FirebaseAuth = FirebaseAuth.getInstance().run {
-        if (BuildConfig.FIREBASE_USE_EMULATORS == "true") this.useEmulator("10.0.2.2", 9099)
-
+        if (BuildConfig.FIREBASE_USE_EMULATORS == "true")
+            this.useEmulator(BuildConfig.FIREBASE_AUTH_HOST, BuildConfig.FIREBASE_AUTH_PORT)
         this
     }
     val database: FirebaseDatabase = FirebaseDatabase.getInstance().run {
-        if (BuildConfig.FIREBASE_USE_EMULATORS == "true") this.useEmulator("10.0.2.2", 9000)
-
+        if (BuildConfig.FIREBASE_USE_EMULATORS == "true")
+            this.useEmulator(FIREBASE_DATABASE_HOST, FIREBASE_DATABASE_PORT)
         this
     }
 
     val storage: FirebaseStorage = FirebaseStorage.getInstance().run {
-        if (BuildConfig.FIREBASE_USE_EMULATORS == "true") this.useEmulator("10.0.2.2", 9199)
-
+        if (BuildConfig.FIREBASE_USE_EMULATORS == "true")
+            this.useEmulator(FIREBASE_STORAGE_HOST, FIREBASE_STORAGE_PORT)
         this
     }
 }
