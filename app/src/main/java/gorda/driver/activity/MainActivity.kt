9792aa234b6246ea2f2897fae018a09b2faaa2d8
DevAlexandreCR <devalexandrecr@gmail.com>
2023-02-03 11:38:53 -0500

    fix: resolve service disapear error

--- app/src/main/java/gorda/driver/activity/MainActivity.kt
+++ app/src/main/java/gorda/driver/activity/MainActivity.kt
@@ -101,9 +101,8 @@ class MainActivity : AppCompatActivity() {
         val navView: NavigationView = binding.navView
         navController = findNavController(R.id.nav_host_fragment_content_main)
         appBarConfiguration = AppBarConfiguration(
-            setOf(
-                R.id.nav_home, R.id.nav_profile, R.id.nav_current_service, R.id.nav_about
-            ), drawerLayout
+            setOf(R.id.nav_current_service, R.id.nav_home),
+            drawerLayout
         )
         setupActionBarWithNavController(navController, appBarConfiguration)
         navView.setupWithNavController(navController)
@@ -121,6 +120,14 @@ class MainActivity : AppCompatActivity() {
             NavigationUI.onNavDestinationSelected(item, navController)
             drawerLayout.closeDrawer(GravityCompat.START)
             true
+        }
+
+        navController.addOnDestinationChangedListener { controller, destination, _ ->
+            if (destination.id == R.id.nav_home) {
+                if (viewModel.currentService.value != null) {
+                    controller.navigate(R.id.nav_current_service)
+                }
+            }
         }
 
         this.switchConnect = binding.appBarMain.toolbar.findViewById(R.id.switchConnect)
@@ -367,6 +374,9 @@ class MainActivity : AppCompatActivity() {
     override fun onBackPressed() {
         if (navController.currentDestination != null && navController.currentDestination?.id != R.id.nav_home) {
             super.onBackPressed()
+            if (viewModel.currentService.value != null) {
+                navController.navigate(R.id.nav_current_service)
+            }
         }
     }
 }
