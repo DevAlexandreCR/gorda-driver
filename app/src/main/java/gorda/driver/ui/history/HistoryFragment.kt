4d774a09634015ab8936669e29dcd8916c4bfba0
alexandrecr <devalexandrecr@gmail.com>
2024-04-14 18:05:17 -0500

    feat: add firestore

--- app/src/main/java/gorda/driver/ui/history/HistoryFragment.kt
+++ app/src/main/java/gorda/driver/ui/history/HistoryFragment.kt
@@ -2,7 +2,6 @@ package gorda.driver.ui.history
 
 import android.os.Bundle
 import androidx.fragment.app.Fragment
-import androidx.recyclerview.widget.GridLayoutManager
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import android.view.LayoutInflater
@@ -10,19 +9,13 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.viewModels
 import gorda.driver.R
-import gorda.driver.ui.history.placeholder.PlaceholderContent
 
 class HistoryFragment : Fragment() {
 
-    private var columnCount = 1
     private val viewmodel: HistoryViewModel by viewModels()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
-        arguments?.let {
-            columnCount = it.getInt(ARG_COLUMN_COUNT)
-        }
 
         viewmodel.getServices()
     }
@@ -42,18 +35,5 @@ class HistoryFragment : Fragment() {
             }
         }
         return view
-    }
-
-    companion object {
-
-        const val ARG_COLUMN_COUNT = "column-count"
-
-        @JvmStatic
-        fun newInstance(columnCount: Int) =
-            HistoryFragment().apply {
-                arguments = Bundle().apply {
-                    putInt(ARG_COLUMN_COUNT, columnCount)
-                }
-            }
     }
 }
