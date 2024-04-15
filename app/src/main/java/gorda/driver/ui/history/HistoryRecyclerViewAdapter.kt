e14477afcf8eb5e26044be1b6b478a313722f49c
alexandrecr <devalexandrecr@gmail.com>
2024-04-14 19:42:45 -0500

    style: add list view

--- app/src/main/java/gorda/driver/ui/history/HistoryRecyclerViewAdapter.kt
+++ app/src/main/java/gorda/driver/ui/history/HistoryRecyclerViewAdapter.kt
@@ -1,13 +1,14 @@
 package gorda.driver.ui.history
 
+import android.content.Context
 import androidx.recyclerview.widget.RecyclerView
 import android.view.LayoutInflater
 import android.view.ViewGroup
 import android.widget.TextView
 import gorda.driver.R
-
 import gorda.driver.databinding.FragmentHistoryBinding
 import gorda.driver.models.Service
+import java.util.Calendar
 
 class HistoryRecyclerViewAdapter(
     private val values: MutableList<Service>
@@ -25,22 +26,32 @@ class HistoryRecyclerViewAdapter(
 
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
         val item = values[position]
-        holder.idView.text = item.id
+        holder.createdAt.text = formatTimeFromSeconds(item.created_at)
         holder.place.text = item.start_loc.name
-        holder.time.text = item.created_at.toString()
-        holder.status.text = item.status
-        holder.price.text = holder.itemView.context.getString(R.string.AmountCurrency, item.amount.toString())
+        holder.status.text = formatStatus(item.status, holder.itemView.context)
+    }
+
+    private fun formatStatus(status: String, context: Context): String {
+        return if (status == Service.STATUS_TERMINATED) context.getString(R.string.completed)
+        else context.getString(R.string.canceled)
+    }
+    private fun formatTimeFromSeconds(currentTimeInSeconds: Long): String {
+        val calendar = Calendar.getInstance()
+        calendar.timeInMillis = currentTimeInSeconds * 1000
+        return "%02d:%02d:%02d".format(
+            calendar.get(Calendar.HOUR_OF_DAY),
+            calendar.get(Calendar.MINUTE),
+            calendar.get(Calendar.SECOND)
+        )
     }
 
     override fun getItemCount(): Int = values.size
 
     inner class ViewHolder(binding: FragmentHistoryBinding) :
         RecyclerView.ViewHolder(binding.root) {
-        val idView: TextView = binding.itemNumber
+        val createdAt: TextView = binding.time
         val place: TextView = binding.place
-        val time: TextView = binding.time
         val status: TextView = binding.status
-        val price: TextView = binding.price
     }
 
 }
