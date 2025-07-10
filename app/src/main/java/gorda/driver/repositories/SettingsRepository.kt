35cba450f0fd311d3c98a1885519bc1d129f6bc6
DevAlexandreCR <devalexandrecr@gmail.com>
2025-07-09 22:46:28 -0500

    feat: add feeMultiplier to RideFeesDeserializer for backend integration

--- app/src/main/java/gorda/driver/repositories/SettingsRepository.kt
+++ app/src/main/java/gorda/driver/repositories/SettingsRepository.kt
@@ -1,11 +1,28 @@
 package gorda.driver.repositories
 
-import com.google.android.gms.tasks.Task
+import android.util.Log
 import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.DatabaseError
+import com.google.firebase.database.ValueEventListener
+import gorda.driver.interfaces.RideFees
+import gorda.driver.serializers.RideFeesDeserializer
 import gorda.driver.services.firebase.Database
 
 object SettingsRepository {
-    fun getRideFees(): Task<DataSnapshot> {
-        return Database.dbRideFees().get()
+    fun getRideFees(callback: (fees: RideFees) -> Unit): Unit {
+        Database.dbRideFees().addValueEventListener(
+            object : ValueEventListener {
+                override fun onDataChange(snapshot: DataSnapshot) {
+                    if (snapshot.exists()) {
+                        val fees = RideFeesDeserializer.getRideFees(snapshot)
+                        callback(fees)
+                        Log.d('f'.toString(), "Ride fees fetched successfully: $fees")
+                    }
+                }
+                override fun onCancelled(error: DatabaseError) {
+                    // Handle error if needed
+                }
+            }
+        )
     }
 }
