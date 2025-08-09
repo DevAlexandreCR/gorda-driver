29530fff9780a5cc1b27786a602e0507e8f01ee4
DevAlexandreCR <devalexandrecr@gmail.com>
2025-08-09 14:38:14 -0500

    feat: refactor distance calculation methods to return Float and improve formatting in Map and MapFragment

--- app/src/main/java/gorda/driver/maps/Map.kt
+++ app/src/main/java/gorda/driver/maps/Map.kt
@@ -1,43 +1,50 @@
 package gorda.driver.maps
 
+import android.annotation.SuppressLint
 import android.graphics.Color
 import android.location.Location
 import com.google.android.gms.maps.model.LatLng
 import com.google.android.gms.maps.model.PolylineOptions
 
-class Map {
+object Map {
+    private val tmpDistance = FloatArray(1)
 
-    companion object {
-        fun calculateDistance(latLng1: LatLng, latLng2: LatLng): Int {
-            val startLoc = Location("last")
-            startLoc.latitude = latLng1.latitude
-            startLoc.longitude = latLng1.longitude
-            val endLoc = Location("last")
-            endLoc.latitude = latLng2.latitude
-            endLoc.longitude = latLng2.longitude
+    fun calculateDistance(latLng1: LatLng, latLng2: LatLng): Float {
+        val startLoc = Location("last")
+        startLoc.latitude = latLng1.latitude
+        startLoc.longitude = latLng1.longitude
+        val endLoc = Location("last")
+        endLoc.latitude = latLng2.latitude
+        endLoc.longitude = latLng2.longitude
 
-            return startLoc.distanceTo(endLoc).toInt()
-        }
+        Location.distanceBetween(
+            startLoc.latitude, startLoc.longitude,
+            endLoc.latitude, endLoc.longitude,
+            tmpDistance,
+        )
 
-        fun distanceToString(distance: Int): String {
-            return if (distance > 1000) (distance / 1000).toString() + "km"
-            else distance.toString() + "m"
-        }
+        return tmpDistance[0]
+    }
 
-        fun calculateTime(distance: Int): Int {
-            return (distance / 5)
-        }
+    @SuppressLint("DefaultLocale")
+    fun distanceToString(distance: Float): String {
+        return if (distance > 1000) String.format("%.2f", (distance / 1000)) + "km"
+        else String.format("%.2f", distance) + "m"
+    }
 
-        fun getTimeString(time: Int): String {
-            return if (time < 60) {
-                 "1m"
-            } else {
-                (time / 60).toString() + "m"
-            }
+    fun calculateTime(distance: Float): Float {
+        return (distance / 5)
+    }
+
+    fun getTimeString(time: Float): String {
+        return if (time < 60) {
+            "1m"
+        } else {
+            (time / 60).toString() + "m"
         }
     }
 
-    fun getDirectionURL(origin: LatLng, dest:LatLng, secret: String) : String{
+    fun getDirectionURL(origin: LatLng, dest: LatLng, secret: String): String {
         return "directions/json?origin=${origin.latitude},${origin.longitude}" +
                 "&destination=${dest.latitude},${dest.longitude}" +
                 "&sensor=false" +
@@ -46,7 +53,7 @@ class Map {
     }
 
     fun makePolylineOptions(routes: ArrayList<Routes>): PolylineOptions {
-        val path =  ArrayList<LatLng>()
+        val path = ArrayList<LatLng>()
         for (i in 0 until routes[0].legs[0].steps.size) {
             val decoded = decodePolyline(routes[0].legs[0].steps[i].polyline.points)
             path.addAll(decoded)
@@ -86,9 +93,11 @@ class Map {
             } while (b >= 0x20)
             val dlng = if (result and 1 != 0) (result shr 1).inv() else result shr 1
             lng += dlng
-            val latLng = LatLng((lat.toDouble() / 1E5),(lng.toDouble() / 1E5))
+            val latLng = LatLng((lat.toDouble() / 1E5), (lng.toDouble() / 1E5))
             poly.add(latLng)
         }
+
         return poly
     }
+
 }
