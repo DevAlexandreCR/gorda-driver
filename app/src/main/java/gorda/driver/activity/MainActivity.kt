e2dc980e88395447b75d434f6845d770447f4428
DevAlexandreCR <devalexandrecr@gmail.com>
2025-08-12 18:36:52 -0500

    feat: enhance disclosure layout with responsive design and new features

--- app/src/main/java/gorda/driver/activity/MainActivity.kt
+++ app/src/main/java/gorda/driver/activity/MainActivity.kt
@@ -392,9 +392,9 @@ class MainActivity : AppCompatActivity() {
         alertDialog.setCancelable(false)
         alertDialog.setOnShowListener {
             alertDialog.getButton(AlertDialog.BUTTON_POSITIVE)
-                .setTextColor(resources.getColor(R.color.primary_light, null))
+                .setTextColor(resources.getColor(R.color.secondary, null))
             alertDialog.getButton(AlertDialog.BUTTON_NEGATIVE)
-                .setTextColor(resources.getColor(R.color.primary_light, null))
+                .setTextColor(resources.getColor(R.color.red, null))
         }
         alertDialog.show()
     }
